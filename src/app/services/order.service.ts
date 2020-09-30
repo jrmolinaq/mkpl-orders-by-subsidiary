34ecd684764c90d569bd5b1464ca30dfe0819b16
Jose Molina <josemolina@subocol.com>
2020-09-30 09:54:36 -0500

    ultimos cambios

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -20,7 +20,7 @@ export class OrderService {
       .set('status', status)
       .set('page', page.toString())
       .set('limit', limit.toString());
-    return this.http.get<Order>(`http://localhost:8080/o/ProviderCompraDigitalPortlet/api/notice/subsidiary/${subsidiaryId}`, { params })
+    return this.http.get<Order>(`/o/ProviderCompraDigitalPortlet/api/notice/subsidiary/${subsidiaryId}`, { params })
       .pipe(
         map(({ content, ...dataPaginator }) => {
           return {
@@ -33,22 +33,22 @@ export class OrderService {
 
   // TODO service
   getOrder(id: any) {
-    return this.http.get<Order>(`http://localhost:8080/o/ProviderCompraDigitalPortlet/api/order/detail/${id}`);
+    return this.http.get<Order>(`/o/ProviderCompraDigitalPortlet/api/order/detail/${id}`);
   }
 
   // TODO service
   acceptOrder(id: any, body: any) {
-    return this.http.put<OrderContent>(`http://localhost:8080/o/ProviderCompraDigitalPortlet/api/order/detail/${id}`, body);
+    return this.http.put<OrderContent>(`/o/ProviderCompraDigitalPortlet/api/order/detail/${id}`, body);
   }
 
   // TODO service
   getCriticalOrders() {
-    return this.http.get<Order[]>(`http://localhost:8080/o/ProviderCompraDigitalPortlet/api/order/critical`);
+    return this.http.get<Order[]>(`/o/ProviderCompraDigitalPortlet/api/order/critical`);
   }
 
   // TODO service
   dispatchOrder(id: any, body: any) {
-    return this.http.put(`http://localhost:8080/o/ProviderCompraDigitalPortlet/api/order/dispatch/${id}`, body);
+    return this.http.put(`/o/ProviderCompraDigitalPortlet/api/order/dispatch/${id}`, body);
   }
 
   // TODO service
@@ -57,7 +57,7 @@ export class OrderService {
         .set('reference', reference)
         .set('amount', amount.toString());
 
-      this.http.get<any>(`http://localhost:8080/o/ProviderCompraDigitalPortlet/api/product/upd/${subsidiaryId}`, { params })
+      this.http.get<any>(`/o/ProviderCompraDigitalPortlet/api/product/upd/${subsidiaryId}`, { params })
         .subscribe();
   }
 
@@ -88,7 +88,7 @@ export class OrderService {
       
       const params = new HttpParams().set('url', "test-inventario.xlsx").set('id_subsidiary', "1");
       
-      this.http.post<any>(`http://localhost:8080/o/SendFileMKPLPortlet/sendfile/inventory`, {
+      this.http.post<any>(`/o/SendFileMKPLPortlet/sendfile/inventory`, {
         "url": "test-inventario.xlsx",
         "id_subsidiary": "1"
       })
